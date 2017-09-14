be1a210b471d355e785cee63c36e56921305f710
selma <selma@gmail.com>
2017-09-14 16:24:25 +0200

    mis a jour le portfolio

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Mon Portfolio</title>
+
+    <!-- lien Bootstrap avec CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">-->
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+    <link href="css/creative.css" rel="stylesheet">
+    <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><em>Selma Muzevic</em></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#about">A propos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">Projets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#portfolio">Parcours</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header class="masthead">
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="homeHeading">Selma Muzevic</h1>
+                <hr>
+                <p>Developpeuse Web Junior</p>
+                <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">A Decouvrir !</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary1">
+        <div class="primary+">
+
+            <img id="selma" src="selma.jpg" alt="Selma Muzevic" height="200px" width="200px">
+        </div>
+
+        <div class="paragraph">
+            <p><strong><font size="5"><em>Je suis en pleine reconversion professionnelle.
+                 Très motivée, j'aime apprendre et réapprendre.
+                 Intéressée par le web, je cherche des nouvelles expériences.
+                 En voulant découvrir encore plus le monde du web,
+                 je me suis orientée vers le développement.</em></font></strong></p>
+        </div>
+    </section>
+
+    <div class="titre_page">
+        <h2>Mon Parcours</h2>
+    </div>
+
+    <section class="parcours">
+
+        <section class="competences">
+            <h4>Competences :</h4>
+            <h4><em>En Développement:</em></h4>
+            <div id="display_langage">
+
+                <h3>Front-end:</h3>
+                <div class="langage">
+                    <p>
+                        <img src="HTML5.png" alt="html5" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="css3.png" alt="css3" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="js.png" alt="javascript" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="sass.png" alt="sass" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="JQuery.png" alt="jquery" width="50px" height="80px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="bootstrap.png" alt="bootstrap" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="angular-logo.png" alt="angular" width="80px" height="80px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="wordpress.png" alt="wordpress" width="50px" height="50px">
+                    </p>
+                </div>
+
+                <h4>Back-end:</h4>
+
+                <div class="langage">
+                    <p>
+                        <img src="php-512.png" alt="php" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="mysql.png" alt="mysql" width="50px" height="50px">
+                    </p>
+                </div>
+                <div class="langage">
+                    <p>
+                        <img src="nodejs_logo.png" alt="nodejs" width="50px" height="50px">
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="experiences">
+            <h3>Experiences Professionnelles:</h3>
+            <p><strong>Avril - Novembre 2017: </strong>Creation des petits projets, realisés dans le cadre de formation en developpement Web / Simplon.co</p>
+            <p><a href="https://github.com/SelmaMuzevic">github.com/SelmaMuzevic/</a></p>
+            <p><strong>2015:</strong> Intérim - Patisserie Pignol / Brignais (69)</p>
+            <p><strong>2011 - 2015:</strong> Hotesse de caisse / Centre Leclerc - 69330 Meyzieu</p>
+            <p><strong>2011:</strong> Vendeuse en Manège à Bijoux / Centre Leclerc - 69330 Meyzieu</p>
+        </section>
+        <section class="formation">
+            <h3>Formation</h3>
+            <p><strong>Avril - Novembre 2017: </strong> Formation en Developpement Web / Simplon.co, Villeurbanne</p>
+            <p><strong>2003 : </strong> Dimplome en secretariat, Université de Zagreb(CRO)</p>
+            <p><strong>2001 - 2003 :</strong> Deux annéés Universitaires en informatique(niveau), Université de Zagreb(CRO)</p>
+        </section>
+        <section class="centre_interet">
+            <h3>Mon Centre d'Interet:</h3>
+            <ul>
+                <li>Danse</li>
+                <li>Creation des bijoux</li>
+            </ul>
+        </section>
+    </section>
+
+    <section id="projets">
+         <?php
+
+$tableau = scandir(".");
+
+foreach($tableau as $fichier){
+    if(!is_dir($fichier)){
+        continue;
+    }
+    if($fichier == "."){
+        continue;
+    }
+    if($fichier == ".."){
+        continue;
+    }
+
+    echo '<li><a href = "'.$fichier.'" > '.$fichier.'</a></li>';
+}
+        ?>
+        <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Mes projets</h2>
+            <p><em>Realisés dans le cadre de la formation:</em></p>
+        </div>
+        <aside id="mes_projets">
+            <div class="miniature" id="miniature_projets1">
+                <img src="" alt="">
+                <a href="">Premier Projet</a> Project Name Category
+            </div>
+
+            <div class="miniature" id="miniature_projets1">
+                <img src="" alt="">
+                <a href="">Premier Projet</a> Project Name Category
+            </div>
+
+            <div class="miniature" id="miniature_projets1">
+                <img src="" alt="">
+                <a href="">Premier Projet</a> Project Name Category
+            </div>
+
+            <div class="miniature" id="miniature_projets1">
+                <img src="" alt="">
+                <a href="">Premier Projet</a> Project Name Category
+            </div>
+            <div class="miniature" id="miniature_projets1">
+                <img src="" alt="">
+                <a href="">Premier Projet</a> Project Name Category
+            </div>
+        </aside>
+    </section>
+
+    <!--<div class="call-to-action bg-dark">
+        <div class="container text-center">
+            <h2></h2>
+            <a class="btn btn-default btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/"></a>
+        </div>
+    </div>-->
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <h2 class="section-heading">Contactez-moi !</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 ml-auto text-center">
+                    <i class="fa fa-phone fa-3x sr-contact"></i>
+                    <p>06.33.90.02.29</p>
+                </div>
+                <div class="col-lg-4 mr-auto text-center">
+                    <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+                    <p>
+                        <a href="muzevicselma34@gmail.com">muzevicselma34@gmail.com</a>
+                    </p>
+                </div>
+
+                <div class="col-lg-4 mr-auto text-center">
+                    <a href="https://linkedin.com/in/selma-muzevic/"><img src="linkedIN.png" alt="linkedin" height="100px" width="100px"></a>
+                </div>
+                <div class="col-lg-4 mr-auto text-center">
+                    <a href="https://github.com/SelmaMuzevic/"><img src="blackGIT.png" alt="git" height="100px" width="100px"></a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
